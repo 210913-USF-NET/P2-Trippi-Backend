b84392a7eb055c9cd45ab7fb3f6191799ddd5e5d
culexgdubs <d4n13lb3rry@gmail.com>
2021-10-28 17:45:03 -0400

    more bltests

--- Trippi/Tests/BLTests.cs
+++ Trippi/Tests/BLTests.cs
@@ -47,6 +47,76 @@ namespace Tests
             Assert.Equal(2, result.Count);
         }
 
+        //  [Fact]
+        // public async void GetOneUserByIdAsyncShouldReturn()
+        // {
+        //     var mkusers = GetMockUsers();
+        //     var MockyRepo = await new MockRepo().MockGetOneUserByIdAsync(mkusers);
+
+        //     var _bl = new BL(MockyRepo.Object);
+
+        //     var result = await _bl.GetOneUserByIdAsync(2);
+
+        //     Assert.Equal(2, result.Id);
+        // }
+
+        [Fact]
+        public async void GetAllTripInvitesAsyncShouldReturn()
+        {
+            var mkusers = GetMockTripInvites();
+            var MockyRepo = await new MockRepo().MockGetAllTripInvitesAsync(mkusers);
+
+            var _bl = new BL(MockyRepo.Object);
+
+            var result = await _bl.GetAllTripInvitesAsync();
+
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public async void GetAllTripsAsyncShouldReturn()
+        {
+            var mkusers = GetMockTrips();
+            var MockyRepo = await new MockRepo().MockGetAllTripsAsync(mkusers);
+
+            var _bl = new BL(MockyRepo.Object);
+
+            var result = await _bl.GetAllTripsAsync();
+
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public async void GetAllRatingsAsyncShouldReturn()
+        {
+            var mkusers = GetMockRatings();
+            var MockyRepo = await new MockRepo().MockGetAllRatingsAsync(mkusers);
+
+            var _bl = new BL(MockyRepo.Object);
+
+            var result = await _bl.GetAllRatingsAsync();
+
+            Assert.Equal(2, result.Count);
+        }
+
+
+         private async Task<List<Rating>> GetMockRatings()
+        {
+            return new List<Rating>()
+            {
+                new Rating()
+                {
+                    Id = 1,
+                    MyRating = 1
+                },
+                new Rating()
+                {
+                    Id = 2,
+                    MyRating = 5
+                }
+            };
+        }
+
         private async Task<List<User>> GetMockUsers()
         {
             return new List<User>()
@@ -63,7 +133,45 @@ namespace Tests
                 }
             };
         }
+
+        private async Task<List<TripInvites>> GetMockTripInvites()
+        {
+            return new List<TripInvites>()
+            {
+                new TripInvites()
+                {
+                    Id = 1,
+                    TripId = 3
+                },
+                new TripInvites()
+                {
+                    Id = 2,
+                    TripId = 7
+                }
+            };
+
+        }
+
+         private async Task<List<Trip>> GetMockTrips()
+        {
+            return new List<Trip>()
+            {
+                new Trip()
+                {
+                    Id = 1,
+                    RatingId = 3
+                },
+                new Trip()
+                {
+                    Id = 2,
+                    RatingId = 7
+                }
+            };
+
+        }
     }
+
+    
 
     
 
